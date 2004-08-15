962e902a17c2b474c63c9507af968f50cbf22099
Ingy döt Net <ingy@cpan.org>
2004-08-15 05:08:19 +0000

    Import of INGY/Kwiki-Archive-Rcs-0.11 from CPAN.
    gitpan-cpan-distribution: Kwiki-Archive-Rcs
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         INGY/Kwiki-Archive-Rcs-0.11.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Kwiki/Archive/Rcs.pm
+++ lib/Kwiki/Archive/Rcs.pm
@@ -2,95 +2,86 @@ package Kwiki::Archive::Rcs;
 use strict;
 use warnings;
 use Kwiki::Archive '-Base';
-our $VERSION = '0.10';
-
-field 'user_name';
-
-sub register {
-    my $registry = shift;
-    $registry->add(page_store_hook => 'commit');
-}
+our $VERSION = '0.11';
 
 sub file_path {
     my $page_id = shift;
-    $self->plugin_directory . '/' . $self->uri_escape($page_id) . ',v';
+    $self->plugin_directory . '/' . $page_id . ',v';
 }
 
-sub init {
-    $self->use_class('pages');
-    $self->generate 
-      unless @{[io($self->plugin_directory)->all]} > 0;
-}
-
-sub generate {
-    my $dir = $self->plugin_directory;
-    umask 0000;
-    chmod 0777, $dir;
-    $self->user_name('kwiki-install');
-    for my $page ($self->pages->all) {
-        $self->commit($page);
-    }
-}
-    
 sub commit {
     my $page = shift;
+    my $props = $self->page_properties($page);
     my $rcs_file_path = $self->file_path($page->id);
     if (not -f $rcs_file_path) {
         $self->shell("rcs -q -i $rcs_file_path < /dev/null");
     }
-    my $time = time;
-    my $msg = join ',',
-      $self->uri_escape($self->user_name || $page->metadata->edit_by),
-      ($page->metadata->edit_time || scalar(gmtime($time))),
-      ($page->metadata->edit_unixtime || $time),
-      ;
 
-    my $page_file_path = $page->database_directory . '/' . $page->id;
+    my $msg = join ',',
+      $self->uri_escape($props->{edit_by}),
+      $props->{edit_time},
+      $props->{edit_unixtime};
+
+    my $page_file_path = $page->io;
     $self->shell(qq{ci -q -l -m"$msg" $page_file_path $rcs_file_path});
+}
+
+sub revision_numbers {
+    my $page = shift;
+    [map $_->{revision_id}, @{$self->history($page)}];
+}
+
+sub fetch_metadata {
+    my $page = shift;
+    my $rev = shift;
+    my $rcs_file_path = $self->file_path($page->id);
+    my $rlog = io("rlog -zLT -r $rev $rcs_file_path |") or die $!; 
+    $rlog->utf8 if $self->use_utf8;
+    $self->parse_metadata($rlog->all);
+}
+
+sub parse_metadata {
+    my $log = shift;
+    $log =~ /
+        ^revision\s+(\S+).*?
+        ^date:\s+(.+?);.*?\n
+        (.*)
+    /xms or die "Couldn't parse rlog:\n$log";
+
+    my $revision_id = $1;
+    my $msg = $3;
+    chomp $msg;
+
+    my ($edit_by, $edit_time, $edit_unixtime) = split ',', $msg;
+    $edit_time ||= $2;
+    $edit_unixtime ||= 0;
+    $revision_id =~ s/^1\.//;
+
+    return {
+        revision_id => $revision_id,
+        edit_by => $self->uri_unescape($edit_by),
+        edit_time => $edit_time,
+        edit_unixtime => $edit_unixtime,
+    };
 }
 
 sub history {
     my $page = shift;
-    my $page_id = $page->id;
-    my $rcs_file_path = $self->file_path($page_id);
-    open RLOG, "rlog -zLT $rcs_file_path |"
-      or DIE $!; 
-    binmode(RLOG, ':utf8') if $self->use_utf8;
-    my $input;
-    {
-        local $/;
-        $input = <RLOG>;
-    }
-    close RLOG;
-    (my $rlog = $input) =~ s/\n=+$.*\Z//ms;
-    my @rlog = split /^-+\n/m, $rlog;
-    shift(@rlog);
-    my $history = [];
-    for (@rlog) {
-        /^revision\s+(\S+).*?
-         ^date:\s+(.+?);.*?\n
-         (.*)
-        /xms or die "Couldn't parse rlog for '$page_id':\n$rlog";
-        my $revision_id = $1;
-        my $msg = $3;
-        chomp $msg;
-        my ($edit_by, $edit_time, $edit_unixtime) = split ',', $msg;
-        $edit_time ||= $2;
-        $edit_unixtime ||= 0;
-        $revision_id =~ s/^1\.//;
-        push @$history,
-          {
-            revision_id => $revision_id,
-            edit_by => $self->uri_unescape($edit_by),
-            edit_time => $edit_time,
-            edit_unixtime => $edit_unixtime,
-          };
-    }
-    return $history;
-}
+    my $rcs_file_path = $self->file_path($page->id);
+    my $rlog = io("rlog -zLT $rcs_file_path |") or die $!; 
+    $rlog->utf8 if $self->use_utf8;
 
-sub revision_number {
-    $self->history(shift)->[0]->{revision_id} || 0;    
+    my $input = $rlog->all;
+    $input =~ s/
+        \n=+$
+        .*\Z
+    //msx;
+    my @rlog = split /^-+\n/m, $input;
+    shift(@rlog);
+
+    return [
+        map $self->parse_metadata($_), @rlog
+    ];
 }
 
 sub fetch {
